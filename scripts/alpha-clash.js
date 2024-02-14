34c7cfe39a93d99a2de8b42d1a1a029baba6e18e
tamimtarik <tamimtarik15@gmail.com>
2024-02-14 22:04:42 +0600

    Score Board update

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -9,6 +9,47 @@
 //     playgroundSection.classList.remove('hidden')
 // }
 
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    console.log('Player Pressed', playerPressed);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+
+    if(playerPressed === expectedAlphabet){
+        console.log('You get a point');
+        console.log('You have pressed correctly', expectedAlphabet)
+        // update score
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+        // 2. increase the score by 1
+        const newScore = currentScore + 1;
+
+        // 3. show the updated score
+        currentScoreElement.innerText = newScore;
+        // start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you missed you lost a life');
+        // step-1: get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+        // step-2: reduce the life count
+        const newLife = currentLife -1;
+        // step-3: display the updated life count
+        currentLifeElement.innerText = newLife;
+    }
+}
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
 function continueGame(){
     // step-1: generate a random alphabet
     const alphabet = getARandomAlphabet();
