e6cd5900325cff0df3abe4edf0b894da3c9d7117
tamimtarik <tamimtarik15@gmail.com>
2024-02-14 10:41:19 +0600

    final push

---
+++ scripts/ultility.js
@@ -0,0 +1,28 @@
+function hideElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById (elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function getARandomAlphabet(){
+    // get or create an alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    // get a random index between 0 - 25
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet);
+    return alphabet;
+}
